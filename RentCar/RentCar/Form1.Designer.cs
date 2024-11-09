d6e4d3ad012e748c8a82d921fbc5b2717c708c8c
csuzart <115844037+csuzart@users.noreply.github.com>
2024-11-09 19:52:54 +0000

    "primeiro commit - login"
    login - primeira parte

---
+++ RentCar/RentCar/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace RentCar
+{
+    partial class txtLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(380, 75);
+            this.lblLogin.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(294, 42);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "LOCAR - LOGIN";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(168, 227);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(92, 36);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(168, 409);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(147, 36);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(358, 214);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(431, 49);
+            this.txtname.TabIndex = 3;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(358, 396);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(431, 49);
+            this.txtpassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.MediumBlue;
+            this.btnLogin.Location = new System.Drawing.Point(490, 512);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(184, 94);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // txtLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 42F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1105, 729);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblLogin);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.Name = "txtLogin";
+            this.Text = "RentCart - Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button btnLogin;
+
+        public EventHandler Form1_Load { get; private set; }
+    }
+}
+
---
+++ RentCar/RentCar/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentCar
+{
+    public partial class txtLogin : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+
+        public txtLogin()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtname.Text;
+            string password = txtpassword.Text;
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM login where username='" + txtname.Text + "' AND password='" + txtpassword.Text + "'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Login efetuado com sucesso!");
+                Form carros = new Form();
+                carros.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login please check username and password");
+            }
+            con.Close();
+        }
+    }
+}
+
+
+        
